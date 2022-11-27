0fbaee4b9153510918cd2fb178c13b87f0d9c144
Diana Chistyakova <103204655+Shirayuky@users.noreply.github.com>
2022-11-27 22:54:38 +0300

    Initial commit

---
+++ ThePersons/gun.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Gun:
+    def __init__(self, screen):
+        """Инициализация пушки"""
+        self.screen = screen
+        self.image = pygame.image.load('Material/img/pixil-frame-0.png')
+
+        # Подучаем картинку на прямоугольник, чтобы в дальнейшем управлять ею
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.mright = False
+        self.mleft = False
+
+    def out_put(self):
+        """Рисует пушку"""
+        # В скобках: (что мы отрисовавыем?, как что?)
+        self.screen.blit(self.image, self.rect)
+
+    def update_gun(self):
+        """Обновляет позицию пушки"""
+        if self.mright and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        elif self.mleft and self.rect.left > 0:
+            self.rect.centerx += -1
